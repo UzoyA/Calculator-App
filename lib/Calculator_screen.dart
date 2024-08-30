f796dbf619d198bc5ad58e6e2f18c834807486db
Noah886619 <ehimarenoah@gmail.com>
2024-08-30 15:10:15 +0100

    calculator app made responsive

--- lib/Calculator_screen.dart
+++ lib/Calculator_screen.dart
@@ -34,7 +34,8 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                     "$number1$operand$number2".isEmpty
                         ? "0"
                         : "$number1$operand$number2",
-                    style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+                    style: const TextStyle(
+                        fontSize: 70, fontWeight: FontWeight.bold),
                     textAlign: TextAlign.end,
                   ),
                 ),
@@ -88,43 +89,153 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
 
   // ####################
   void onBtnTap(String values) {
+    if (values == Btn.clr) {
+      clear();
+      return;
+    }
+
+    if (values == Btn.ans) {
+      toggleSign();
+      return;
+    }
+
+    if (values == Btn.per) {
+      convertToPercentage();
+      return;
+    }
+
+    if (values == Btn.calculate) {
+      calculate();
+      return;
+    }
+
+    appendvalues(values);
+  }
+
+  // ###########################################
+  // calculate the result
+  void calculate() {
+    if (number1.isEmpty || operand.isEmpty || number2.isEmpty) return;
+
+    final double num1 = double.parse(number1);
+    final double num2 = double.parse(number2);
+
+    var result = 0.0;
+
+    switch (operand) {
+      case Btn.add:
+        result = num1 + num2;
+        break;
+      case Btn.subtract:
+        result = num1 - num2;
+        break;
+      case Btn.multiply:
+        result = num1 * num2;
+        break;
+      case Btn.divide:
+        result = num1 / num2;
+        break;
+      default:
+    }
+
+    setState(() {
+      number1 = '$result';
+      if (number1.endsWith('.0')) {
+        number1 = number1.substring(0, number1.length - 2);
+      }
+      operand = '';
+      number2 = '';
+    });
+  }
+
+  // ###################
+  // converts output to %
+  void convertToPercentage() {
+    // ex: 434+324
+    if (number1.isNotEmpty && operand.isNotEmpty && number2.isNotEmpty) {
+      // calculate before conversion
+      // TODO
+      //
+    }
+
+    if (operand.isNotEmpty) {
+      // cannot be converted
+      return;
+    }
+    final number = double.parse(number1);
+    setState(() {
+      number1 = "${(number / 100)}";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  // ##################
+  void toggleSign() {
+    setState(() {
+      if (operand.isEmpty && number1.isNotEmpty) {
+        number1 = number1.startsWith('-') ? number1.substring(1) : '-$number1';
+      } else if (number2.isNotEmpty) {
+        number2 = number2.startsWith('-') ? number2.substring(1) : '-$number2';
+      }
+    });
+  }
+
+  // ###################
+  // delete one from the end
+  void clear() {
+    if (number2.isNotEmpty) {
+      // 12323 => 1232
+      number2 = number2.substring(0, number2.length - 1);
+    } else if (operand.isNotEmpty) {
+      operand = "";
+    } else if (number1.isNotEmpty) {
+      number1 = number1.substring(0, number1.length - 1);
+    }
+
+    setState(() {});
+  }
+
+  // ########################
+  // appendvalues to the end
+  void appendvalues(String values) {
     // number1 operand number2
     //  234      +       5678
 
+    // if is operand and not "."
     if (values != Btn.dot && int.tryParse(values) == null) {
-      //
+      // operand pressed
       if (operand.isNotEmpty && number2.isNotEmpty) {
         // TODO calculate the equation before assigning new operand
       }
       operand = values;
+      // assign values to number1 veriable
     } else if (number1.isEmpty || operand.isEmpty) {
-      // number1 = "1.2"
+      // check if values is "."  |  // ex: number1 = "1.2"
       if (values == Btn.dot && number1.contains(Btn.dot)) return;
       if (values == Btn.dot && (number1.isEmpty || number1 == Btn.dot)) {
-        // number1 = "" | "0"
+        // ex: number1 = "" | "0"
         values = "0.";
       }
       number1 += values;
-    } else if (number2.isEmpty || operand.isNotEmpty) {
-      // number1 = "1.2"
+    }
+    // assign values to number2 veriable
+    else if (number2.isEmpty || operand.isNotEmpty) {
+      //  check if values is "."  |  number1 = "1.2"
       if (values == Btn.dot && number2.contains(Btn.dot)) return;
       if (values == Btn.dot && (number2.isEmpty || number2 == Btn.dot)) {
         // number1 = "" | "0"
         values = "0.";
       }
-      number1 += values;
+      number2 += values;
     }
 
-    setState(() {
-      number1 += values;
-      operand += values;
-      number2 += values;
-    });
+    setState(() {});
   }
 
   //  ###################
   Color getBtncolor(values) {
-    return [Btn.clr, Btn.pnm].contains(values)
+    return [Btn.clr, Btn.ans].contains(values)
         ? Colors.blueGrey
         : [
             Btn.per,
--- lib/botton_values.dart
+++ lib/botton_values.dart
@@ -1,11 +1,11 @@
 class Btn {
   static const String clr = "C";
-  static const String pnm = "+/-";
+  static const String ans = "+/-";
   static const String per = "%";
   static const String multiply = "x";
   static const String divide = "÷";
   static const String add = "+";
-  static const String subtract = "−";
+  static const String subtract = "-";
   static const String calculate = "=";
   static const String dot = "•";
 
@@ -22,7 +22,7 @@ class Btn {
 
   static const List<String> buttonValues = [
     clr,
-    pnm,
+    ans,
     per,
     multiply,
     n7,
